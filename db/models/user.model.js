80eaafcf8959f1d553be2e2cfa7c3bb3dfa3e64a
Umang A <umanga.eng@gmail.com>
2023-08-24 08:40:18 -0400

    feat: user model updatd

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose')
+const {v4:uuidv4} = require("uuid")
+const moment = require("moment")
 const Schema = mongoose.Schema
-
 const userSchema = new Schema({
-    id: {type: Number, required: true},
+    id: {type: String, required: false, default: uuidv4()},
     name: {type: String, required: true},
     email: {type: String, required: true},
     phone: {type: Number, required: true},
     admin: {type: Boolean, required: false, default:false},
-    addedOn: {type: String, required: true},
+    addedOn: {type: String, required: false, default: moment().clone().format("YYYYMMDD")},
     active: {type: Boolean, required: false, default: true},
     stats: {
         avgCompletionTime: {type: Number, required: true, default: 0},
