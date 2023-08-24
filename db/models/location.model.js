65b01b3502141352f2607bfdc257daf46f843552
Umang A <umanga.eng@gmail.com>
2023-08-24 08:39:46 -0400

    feat: location schema updated

--- db/models/location.model.js
+++ db/models/location.model.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose')
+const {v4:uuidv4} = require("uuid")
+const moment = require("moment")
 const Schema = mongoose.Schema
 
 
 const locationSchema = new Schema({
-    id: {type: Number, required: true},
+    id: {type: String, required: false, default: uuidv4()},
     name: {type: String, required: true},
-    manager: {type: String, required: true},
-    lists: {type: Array, required: false}
+    manager: {type: Object, required: true},
+    lists: {type: Array, required: false},
+    addedOn: {type:String, required:false, default: moment().clone().format("YYYYMMDD")}
 })
 
 const Location = mongoose.model('Location', locationSchema)
